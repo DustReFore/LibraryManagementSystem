fb5751384442d2d35ebc88efe1d10490839e1621
DustReFore <Artems1357@gmail.com>
2026-06-12 16:52:57 +0200

    added models

--- src/main/java/com/example/first_project/controller/BookController.java
+++ src/main/java/com/example/first_project/controller/BookController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
-    @RequestMapping("/")
-    public String home() {
-        return "home";
+public class BookController {
+    @RequestMapping("/books")
+    public String books() {
+        return "books";
     }
 }
